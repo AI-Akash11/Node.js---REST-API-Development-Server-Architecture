521ecb1d18b863d4437c75ff77dad901d93b71a7
Ali Imam Akash <akash1437aliimam@gmail.com>
2026-07-22 21:28:35 +0600

    Building GET Method Operations

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
+import type { IProduct } from "../types/product.type";
 
 export const productController = (
   req: IncomingMessage,
@@ -7,6 +8,14 @@ export const productController = (
 ) => {
   const url = req.url;
   const method = req.method;
+
+  const ulrParts = url?.split("/");
+  // console.log(ulrParts);
+
+  const id =
+    ulrParts && ulrParts[1] === "products" ? Number(ulrParts[2]) : null;
+
+  // console.log("this is the id", id);
 
   // const products = [
   //   { id: 1, name: "Product 1", price: 10.99 },
@@ -18,6 +27,23 @@ export const productController = (
 
   if (url === "/products" && method === "GET") {
     res.writeHead(200, { "content-type": "application/json" });
-    res.end(JSON.stringify({ message: "This is products route from controller", products }));
+    res.end(
+      JSON.stringify({
+        message: "This is products route from controller",
+        products,
+      }),
+    );
+  } else if (method === "GET" && id !== null) {
+    const products = readProduct();
+    const product = products.find((p: IProduct) => (p.id === id));
+    // console.log(product);
+
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "This is product route from controller",
+        product,
+      }),
+    );
   }
 };
